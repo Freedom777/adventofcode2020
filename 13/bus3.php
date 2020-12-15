20f85c75345ded689a6888878b8849cd050fdd8e
Oleg Liubchenko <oleg.liubchenko@cogify.io>
2020-12-15 19:26:52 +0200

    13.2 progress

--- 13/bus3.php
+++ 13/bus3.php
@@ -1,26 +1,33 @@
 <?php
 
-// $data = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$data = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-$data [1] = '67,7,59,61';
+// $data [1] = '1789,37,47,1889';
+// $data [1] = '10,3';
 $intervals = explode(',', $data [1]);
 foreach ($intervals as $key => $interval) {
     if ($interval == 'x') {
         unset($intervals [$key]);
     }
 }
+array_walk($intervals, function (&$item) {
+    $item = (int) $item;
+});
 
-$sum = 1;
+
+$firstInterval = $intervals [0];
+unset($intervals [0]);
+
+$cur = 0;
+$curValue = 0;
+$curSum = $firstInterval;
 foreach ($intervals as $key => $interval) {
-    var_dump($interval + $key);
-    $sum *= ($interval + $key);
-    var_dump($sum);
+    while ( $curValue != $cur + $key) {
+        $cur += $curSum;
+        $multiplier = (int) ceil($cur / $interval);
+        $curValue = $interval * $multiplier;
+    }
+    $curSum *= $interval;
 }
-var_dump($sum);
-var_dump($sum / 754018);
 
-/*for ($i = 2; $i < 10;$i++) {
-
-}*/
-
-// var_dump($curValue);
+var_dump($cur);

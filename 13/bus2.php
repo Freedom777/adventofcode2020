cdb8eac58186565922e4504a6fabd3464a4e8041
Oleg Liubchenko <oleg.liubchenko@cogify.io>
2020-12-15 20:01:54 +0200

    13 success

--- 13/bus2.php
+++ 13/bus2.php
@@ -2,25 +2,38 @@
 
 $data = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-// $data [1] = '7,13,x,x,59,x,31,19';
+// $data [1] = '7,13,x,x,59,x,31,19'; // 1202161486
+// $data [1] = '67,7,59,61';
 $intervals = explode(',', $data [1]);
 foreach ($intervals as $key => $interval) {
     if ($interval == 'x') {
         unset($intervals [$key]);
     }
 }
+array_walk($intervals, function (&$item) {
+    $item = (int) $item;
+});
 
-$flag = false;
-$i = 0;
-while (!$flag) {
-    $curValue = ++$i * $intervals [0];
-    $flag = true;
-    foreach ($intervals as $key => $interval) {
-        if (($curValue + $key) % $interval != 0) {
-            $flag = false;
-            break;
-        }
-    }
+define('MAX_INTERVAL', max($intervals));
+define('MAX_KEY', array_search(MAX_INTERVAL, $intervals));
+unset($intervals [MAX_KEY]);
+
+$ints = [];
+foreach ($intervals as $key => $interval) {
+    $ints [$key - MAX_KEY] = $interval;
 }
 
-var_dump($curValue);
+
+$cur = 0;
+$curValue = 0;
+$curSum = MAX_INTERVAL;
+foreach ($ints as $key => $interval) {
+    while ( $curValue != $cur + $key) {
+        $cur += $curSum;
+        $multiplier = (int) ceil(($cur + $key) / $interval);
+        $curValue = $interval * $multiplier;
+    }
+    $curSum *= $interval;
+}
+
+var_dump($cur - MAX_KEY);

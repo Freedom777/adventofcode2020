692e75f36d0a71e52cfbd692289fcfcb501907f7
Oleg Liubchenko <oleg.liubchenko@cogify.io>
2020-12-15 21:00:09 +0200

    15 done

--- 15/elves2.php
+++ 15/elves2.php
@@ -1,56 +1,47 @@
 <?php
 
 $data = file('input.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+define('COUNT', 30000000);
 
-// $data[0] = '0,3,6';
+// $data[0] = '3,1,2';
 $data = explode(',', $data[0]);
 
 array_walk($data, function (&$item) {
     $item = (int) $item;
 });
 
-$spokenAr = array_flip($data);
+$spokenCntAr = array_flip($data);
 
 $turnsAr = [];
+$lastSpokenAr = [];
 $currentTurn = 0;
-foreach (array_keys($spokenAr) as $value) {
+foreach (array_keys($spokenCntAr) as $value) {
     $turnsAr [++$currentTurn] = $value;
-    $spokenAr [$value] = 1;
+    $spokenCntAr [$value] = 1;
+    $lastSpokenAr [$value] = $currentTurn;
 }
 
 $flag = false;
 $cnt = 0;
+$prevTurnValue = $data[sizeof($data)-1];
 while (!$flag) {
-    ++$currentTurn;
-    $currentNumber = turn($currentTurn, $turnsAr, $spokenAr);
-    if ($currentTurn == 30000000) {
+    $prevTurn = $currentTurn++;
+
+    $currentValue = 0;
+    if ($spokenCntAr [$prevTurnValue] != 1) {
+        $currentValue = $prevTurn - $lastSpokenAr [$prevTurnValue];
+    }
+
+    if (!isset($spokenCntAr[$currentValue])) {
+        $spokenCntAr [$currentValue] = 0;
+    }
+    ++$spokenCntAr [$currentValue];
+
+    $lastSpokenAr [$prevTurnValue] = $prevTurn;
+    $prevTurnValue = $currentValue;
+    if ($currentTurn == COUNT) {
         $flag = true;
     }
 }
 
-var_dump($currentNumber);
-
-function turn($currentTurn, &$turnsAr, &$spokenAr) {
-    $prevTurnNumber = $turnsAr[$currentTurn - 1];
-    if ($spokenAr [$prevTurnNumber] == 1) {
-        $currentNumber = 0;
-    } else {
-        $ar = [];
-        $i = $currentTurn - 1;
-        while (sizeof($ar) != 2) {
-            if ($turnsAr[$i] == $prevTurnNumber) {
-                $ar [] = $i;
-            }
-            $i--;
-        }
-        $currentNumber = $ar[0] - $ar[1];
-    }
-
-    $turnsAr [$currentTurn] = $currentNumber;
-    if (!isset($spokenAr[$currentNumber])) {
-        $spokenAr [$currentNumber] = 0;
-    }
-    ++$spokenAr [$currentNumber];
-
-    return $currentNumber;
-}
+var_dump($currentValue);
